adc0699e4243470429cab2fdf6553ca024c192f8
Daniel Abraham Granados Infante <danieltec17@hotmail.com>
2018-10-13 13:47:50 -0600

    Clase5

---
+++ PrograAvanzada.NorthWind/PrograAvanzada.NorthWind.Products.Model/MisTablas/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograAvanzada.NorthWind.Products.Model
+{
+   public partial class Products
+    {
+        public Nullable<short> unidadesDeDesabastecimiento {
+
+            get
+            {
+                Nullable<short> elResultado = 0;
+                if (ReorderLevel > UnitsInStock)
+                    elResultado = (short?)(ReorderLevel - UnitsInStock);
+                return elResultado;
+            }
+
+        }
+    }
+}
